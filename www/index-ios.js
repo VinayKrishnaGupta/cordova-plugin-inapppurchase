69be77d19a04e6ebc06c4eeb7286404de674995e
Vinay Krishna Gupta <vinaygupta.iitkgp@gmail.com>
2018-11-13 13:49:41 +0530

    SubscribeWithUserID method added in www/index-ios.js

--- www/index-ios.js
+++ www/index-ios.js
@@ -118,6 +118,9 @@ inAppPurchase.subscribe = function (productId) {
   return inAppPurchase.buy(productId);
 };
 
+inAppPurchase.subscribeWithUserId = (productId, userIdentifier) => {
+  return inAppPurchase.buyWithUserId(productId, userIdentifier);
+}
 /**
  * This function exists so that the iOS plugin API will be compatible with that of Android -
  * where this function is required.
